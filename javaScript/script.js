535f4a075f9f4636aa14df4414186dc7c6ef4ce4
Venkata Satish <sivakoduvenkatasatish@gmail.com>
2025-12-01 12:36:38 +0530

    portfolio

---
+++ javaScript/script.js
@@ -0,0 +1,29 @@
+let myCv = document.querySelector(".MyCV")
+
+myCv.addEventListener("click",function(e){
+    e.preventDefault()
+})
+
+// document.querySelectorAll('.A').forEach(link => {
+//   link.addEventListener('click', function() {
+//     console.log("hai")
+// })});
+
+let time = document.querySelector(".time")
+
+function timeset(){
+
+  let clock = new Date();
+  let hours = clock.getHours();
+  let minutes = clock.getMinutes();
+  let seconds = clock.getSeconds();
+  let AMPM = hours >= 12? "PM" : "AM"
+  hours = hours%12 || 12
+  hours = hours < 10 ? "0"+hours:hours;
+  minutes = minutes < 10 ? "0"+minutes:minutes;
+  seconds = seconds < 10 ? "0"+seconds:seconds;
+  time.innerHTML= `${hours} : ${minutes} : ${seconds} ${AMPM}`
+
+}
+setInterval(timeset,1000)
+timeset()
