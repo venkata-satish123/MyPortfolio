4d54512f8c91ae862052c11cb762e8f846d70250
Venkata Satish <sivakoduvenkatasatish@gmail.com>
2025-12-07 19:17:29 +0530

    portfolio

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,32 +1,19 @@
-let myCv = document.querySelector(".MyCV")
+let time = document.querySelector(".time");
 
-myCv.addEventListener("click",function(e){
-    e.preventDefault()
-})
-
-// document.querySelectorAll('.A').forEach(link => {
-//   link.addEventListener('click', function() {
-//     console.log("hai")
-// })});
-
-let time = document.querySelector(".time")
-
-function timeset(){
-
+function timeset() {
   let clock = new Date();
   let hours = clock.getHours();
   let minutes = clock.getMinutes();
   let seconds = clock.getSeconds();
-  let AMPM = hours >= 12? "PM" : "AM"
-  hours = hours%12 || 12
-  hours = hours < 10 ? "0"+hours:hours;
-  minutes = minutes < 10 ? "0"+minutes:minutes;
-  seconds = seconds < 10 ? "0"+seconds:seconds;
-  time.innerHTML= `${hours} : ${minutes} : ${seconds} ${AMPM}`
-
+  let AMPM = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  time.innerHTML = `${hours} : ${minutes} : ${seconds} ${AMPM}`;
 }
-setInterval(timeset,1000)
-timeset()
+setInterval(timeset, 1000);
+timeset();
 
 // .......menubar,,,,,,,
 
@@ -34,11 +21,11 @@ let ul = document.querySelector("#sidemenu");
 let openmenu = document.querySelector("#menu-icon");
 let closemenu = document.querySelector("#cross-icon");
 
-openmenu.addEventListener("click",function(){
+openmenu.addEventListener("click", function () {
   ul.style.display = "block";
-})
+});
 
-closemenu.addEventListener("click",function(){
+closemenu.addEventListener("click", function () {
   ul.style.display = "none";
-  console.log("hai")
-})
+  console.log("hai");
+});
